5d96a7a9ef9a537abcd45a06221d790c6fa4c49c
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2023-09-21 14:16:57 +0100

    Add script to generate .env files.

---
+++ Scripts/generate-env-files.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+pushd .. >/dev/null || exit
+
+cat << EOF > Apps/recipe-browser/.env
+BROWSER_PORT="5080"
+BROWSER_CRT_FILE="../../Keys/localhost.crt"
+BROWSER_KEY_FILE="../../Keys/localhost.key"
+
+SERVER_URL="https://localhost:5180"
+EOF
+
+cat << EOF > Apps/recipe-server/.env
+CORS_ORIGIN="https://localhost:5080"
+
+SERVER_PORT="5180"
+SERVER_CRT_FILE="../../Keys/localhost.crt"
+SERVER_KEY_FILE="../../Keys/localhost.key"
+
+SESSION_DOMAIN="localhost"
+SESSION_SECRET="the quick brown fox jumped over the lazy dog"
+
+DATABASE_HOST="localhost"
+DATABASE_PORT="53306"
+DATABASE_USERNAME="recipe"
+DATABASE_PASSWORD="Recipe123"
+DATABASE_DATABASE="recipe"
+EOF
+
+cat << EOF > Services/recipe/.env
+DATABASE_HOST="localhost"
+DATABASE_PORT="53306"
+DATABASE_USERNAME="recipe"
+DATABASE_PASSWORD="Recipe123"
+DATABASE_DATABASE="recipe"
+EOF
+
+popd >/dev/null || exit
